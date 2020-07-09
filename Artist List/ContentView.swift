2e53367d94e0e6308f17dd198b11003d5942e255
Milo Wyner <milowyner@gmail.com>
2020-07-09 10:15:56 -0700

    Initial Commit

---
+++ Artist List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Artist List
+//
+//  Created by Milo Wyner on 7/9/20.
+//  Copyright © 2020 Milo Wyner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
