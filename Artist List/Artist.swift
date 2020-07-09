dc293c25db17b22366ededda635602341df3edaa
Milo Wyner <milowyner@gmail.com>
2020-07-09 10:45:21 -0700

    Add Artist.swift which is a struct that can hold an artists name, birth year, city, and country

---
+++ Artist List/Artist.swift
@@ -0,0 +1,16 @@
+//
+//  Artist.swift
+//  Artist List
+//
+//  Created by Milo Wyner on 7/9/20.
+//  Copyright © 2020 Milo Wyner. All rights reserved.
+//
+
+import Foundation
+
+struct Artist {
+    let name: String
+    let birthYear: Int
+    let city: String
+    let country: String
+}
